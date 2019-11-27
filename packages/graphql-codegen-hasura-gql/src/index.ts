936d09e729997019628ea028d132248bf89d76d9
Ahrnee <ahrndee@gmail.com>
2019-11-27 14:44:40 +0000

    --

--- packages/graphql-codegen-hasura-gql/src/index.ts
+++ packages/graphql-codegen-hasura-gql/src/index.ts
@@ -179,3 +179,4 @@ function makeEntityDeleteMutationGql(namedType: GraphQLNamedType, importArray: s
 }
 
 // --------------------------------------
+//
--- packages/graphql-codegen-hasura-typescript/src/index.ts
+++ packages/graphql-codegen-hasura-typescript/src/index.ts
@@ -172,3 +172,4 @@ function makeEntityDeleteMutationGql(namedType: GraphQLNamedType, importArray: s
 }
 
 // --------------------------------------
+//
